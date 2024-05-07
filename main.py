a49f588c65f6496b407a65383dec30d27fff3ac8
vkazakov127 <166021972+vkazakov127@users.noreply.github.com>
2024-05-07 15:29:45 +0300

    Add files via upload
    Домашняя работа по уроку "Пространство имён и способы вызова функции"

---
+++ main.py
@@ -0,0 +1,12 @@
+def test():
+    # a, b — local variables
+    a = "I learn"
+    b = "Python"
+    print(a, b)
+
+def test2(par1, par2, par3='language'):
+    print(par1, par2, par3)
+
+test()
+test2('Python is', 'my new')
+
